8b56477d78b6ae8c35979371f8b9d939e89376ff
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-22 22:39:47 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("SequenciaLimites");
+
+Console.WriteLine("Digite 2 números interiros sendo positivo ou negativo;");
+
+int inicio, fim;
+
+Console.Write("\nInício: ");
+inicio = Convert.ToInt32(Console.ReadLine());
+Console.Write("\nFim: ");
+fim = Convert.ToInt32(Console.ReadLine());
